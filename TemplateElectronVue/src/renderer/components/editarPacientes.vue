b622e19dac2a9eb5b1a2fba4733bd680cb8f4ebc
Ana Hernandez <her17138@uvg.edu.gt>
2019-04-24 16:12:53 -0600

    filtrado de tabla de pacientes funcional (con datos muestra)

--- TemplateElectronVue/src/renderer/components/editarPacientes.vue
+++ TemplateElectronVue/src/renderer/components/editarPacientes.vue
@@ -1,71 +1,85 @@
+
+<link {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}, {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'} >
+
 <template>
   <div class="grey--text text--darken-2">
     <h1 class="text-center">Gestión de Pacientes</h1>
     <div>
       <b-container class="bv-example-row1">
         <b-row align-h="around" align-v="center">
-            <input v-model="search" class="form-control" placeholder="Buscar por nombre, apellido o número CUI">
-            <br>
-            <br>
-            <br>
-            <v-data-table
-                :headers="headers"
-                :items="pacientes"
-                class="elevation-1"
-            >
-                <template v-repeat="pacientes | filterBy search">
-                <td>{{ props.item.id }}</td>
-                <td class="text-xs-center">{{ props.item.nombre }}</td>
-                <td class="text-xs-center">{{ props.item.apellido }}</td>
-                <td class="text-xs-center">{{ props.item.pais_nacimiento }}</td>
-                <td class="text-xs-center">{{ props.item.fecha_nacimiento }}</td>
-                </template>
-            </v-data-table>
-          <b-col order="1" cols="5">
-            <form>
-              <div class="form-group">
-                <br>
-                <br>
-                <br>
-              <h1>Nombre</h1>
-              <h2 class="subheading font-weight-light">  Aqui va el nombre del paciente</h2>
-              </div>
-              <div class="form-group" >
-                <label for="dpiInput">DPI</label>
-                <input id="dpiInput" type="text" v-model="id" class="form-control" placeholder="DPI">
-              </div>
-              <div class="form-group">
-                <label for="exampleInputEmail1">Correo</label>
-                <input
-                  id="emailInput"
-                  type="email"
-                  v-model="email"
-                  class="form-control"
-                  placeholder="Correo Electronico"
-                >
-              </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1">Contraseña</label>
-                <input
-                  id="passwordInput"
-                  type="password"
-                  v-model="password"
-                  class="form-control"
-                  placeholder="Contraseña"
-                >
-              </div>
-              <div>
-                <label for="levelInput">Tipo de usuario</label>
-                <b-form-select id="levelInput" v-model="selected" class="mb-3">
-                  <option value="Doctor">Doctor</option>
-                  <option value="Administrador">Administrador</option>
-                  <option value="Secretaria">Secretaria</option>
-                  <option value="Visitante">Visitante</option>
-                  <option value="Asistente">Asistente</option>
-                </b-form-select>
-              </div>
-            </form>
-          </b-col>
+          <b-col order="2" cols="10">
+                <div class="form-group">
+                    <v-text-field
+                        v-model="search"
+                        label="Búsqueda por nombre, apellido o número de CUI"
+                        outline
+                    ></v-text-field>
+                    <v-data-table
+                        :headers="headers"
+                        :items="pacientes"
+                        :search="search"
+                    >
+                        <template slot="items" slot-scope="props">
+                        <td class="text-xs-center">{{ props.item.id }}</td>
+                        <td class="text-xs-center">{{ props.item.nombre }}</td>
+                        <td class="text-xs-center">{{ props.item.apellido }}</td>
+                        <td class="text-xs-center">{{ props.item.pais_nacimiento }}</td>
+                        <td class="text-xs-center">{{ props.item.fecha_nacimiento }}</td>
+                        </template>
+                        <!-- cuando la busqueda no tenga resultados -->
+                        <template v-slot:no-results>
+                            <v-alert :value="true" color="error">
+                            No se ha encontrado un paciente que tenga "{{ search }}" en su información.
+                            </v-alert>
+                        </template>
+                    </v-data-table>
+            
+                </div>
+                    <form>
+                    <div class="form-group">
+                        <br>
+                        <br>
+                        <br>
+                    <h1>Nombre</h1>
+                    <h2 class="subheading font-weight-light">  Aqui va el nombre del paciente</h2>
+                    </div>
+                    <div class="form-group" >
+                        <label for="dpiInput">DPI</label>
+                        <input id="dpiInput" type="text" v-model="id" class="form-control" placeholder="DPI">
+                    </div>
+                    <div class="form-group">
+                        <label for="emailInput">Correo</label>
+                        <input
+                        id="emailInput"
+                        type="email"
+                        v-model="email"
+                        class="form-control"
+                        placeholder="Correo Electronico"
+                        >
+                    </div>
+                    <div class="form-group">
+                        <label for="passwordInput">Contraseña</label>
+                        <input
+                        id="passwordInput"
+                        type="password"
+                        v-model="password"
+                        class="form-control"
+                        placeholder="Contraseña"
+                        >
+                    </div>
+                    <div>
+                        <label for="levelInput">Tipo de usuario</label>
+                        <b-form-select id="levelInput" v-model="selected" class="mb-3">
+                        <option value="Doctor">Doctor</option>
+                        <option value="Administrador">Administrador</option>
+                        <option value="Secretaria">Secretaria</option>
+                        <option value="Visitante">Visitante</option>
+                        <option value="Asistente">Asistente</option>
+                        </b-form-select>
+                    </div>
+                </form>
+                
+            </b-col>
         </b-row>
       </b-container>
     </div>
@@ -73,13 +87,13 @@
       <b-container class="bv-example-row2">
         <b-row class="justify-content-md-center">
           <b-col>
-            <button type="button" class="btn btn-lg btn-warning btn-block" v-on:click="crear">Crear</button>
+            <!--button type="button" class="btn btn-lg btn-warning btn-block" v-on:click="crear">Crear</button>
           </b-col>
           <b-col>
             <button type="button" class="btn btn-lg btn-warning btn-block" v-on:click="modificar">Modificar</button>
           </b-col>
           <b-col>
-            <button type="button" class="btn btn-lg btn-warning btn-block" v-on:click="eliminar" >Eliminar</button>
+            <button type="button" class="btn btn-lg btn-warning btn-block" v-on:click="eliminar" >Eliminar</button> -->
           </b-col>
         </b-row>
       </b-container>
@@ -90,14 +104,14 @@
 
 <script>
 export default {
-  mounted() {
-    this.$http.get("http://localhost:8000/users").then(response => {
-      this.user = response.data.users;
-    });
-  },
     data () {
       return {
-        search: '',
+        search:'',
+        id:'',
+        name:'',
+        email:'',
+        password:'',
+        selected: null,
         headers: [
           {
             text: 'CUI (ID)',
@@ -184,9 +198,18 @@ export default {
         ]
       }
   },
-  methods: {
-        //aqui va el metodo para llenar la tabla  
-  }
+    methods: {
+        //para filtrar los datos de la tabla
+        eliminar(){
+        this.$http.delete(`http://localhost:8000/users/destroy?id=${this.id}`).then(response=>{});
+        },
+        crear(){
+        this.$http.post(`http://localhost:8000/users/create?id=${this.id}&name=${this.name}&email=${this.email}&password=${this.password}`).then(response=>{});
+        },
+        modificar(){
+        this.$http.put(`http://localhost:8000/users/update?id=${this.id}&name=${this.name}&email=${this.email}&password=${this.password}`).then(response=>{});
+        }
+    }
 };
 </script>
 <style>
--- TemplateElectronVue/src/renderer/main.js
+++ TemplateElectronVue/src/renderer/main.js
@@ -65,6 +65,6 @@ new Vue({
   router,
   render: h => h(App),
   mounted () {
-    this.$router.push('/home')
+    this.$router.push('/page-four')
   }
 }).$mount('#app');
