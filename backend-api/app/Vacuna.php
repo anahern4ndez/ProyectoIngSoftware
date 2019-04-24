90f421b2000b3fc62704392c7ff3f1358f24aad9
Francisco Molina <molinajimenez01@gmail.com>
2019-04-24 11:00:35 -0600

    creacion de params para paciente de vacuna, se actualizo metodo EstadoController@insert

--- backend-api/app/Vacuna.php
+++ backend-api/app/Vacuna.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Vacuna extends Model
 {
     //
+    protected $param=[
+        'idPaciente',
+        'BCG',
+        'Poliovirus',
+        'Hepatitis A',
+        'Hepatitis B',
+        'Neumococo',
+        'Influenza',
+        'DPT',
+        'SPR'
+    ];
+
+    public function idPaciente(){
+        return $this->hasMany('App\Paciente','CUI', 'idPaciente'); 
+    }
 }
