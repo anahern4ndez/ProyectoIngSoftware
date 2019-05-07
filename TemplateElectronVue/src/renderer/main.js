67a49d5443674c807033068a3e981f2cdebffdcc
sebdeveloper6952 <sebdeveloper6952@gmail.com>
2019-05-07 00:01:46 -0600

    agregar ruta de CreacionCitas.

--- TemplateElectronVue/src/renderer/main.js
+++ TemplateElectronVue/src/renderer/main.js
@@ -34,7 +34,6 @@ Vue.use(Vuetify, {
     }
 });
 
-<<<<<<< HEAD
 const routes = [{
         path: '/login',
         component: Vue.component('login', require('./components/Login.vue').default)
@@ -71,45 +70,6 @@ const routes = [{
         path: '/Citas',
         component: Vue.component('creacion-citas', require('./components/CreacionCitas.vue').default)
     }
-=======
-const routes = [
-  {
-    path: '/login',
-    component: Vue.component('login', require('./components/Login.vue').default)
-  },
-  {
-    path: '/page-one',
-    component: Vue.component('page-one', require('./components/PageOne.vue').default)
-  },
-  {
-    path: '/page-two',
-    component: Vue.component('page-two', require('./components/PageTwo.vue').default)
-  },
-  {
-    path: '/page-three',
-    component: Vue.component('page-three', require('./components/PageThree.vue').default)
-  },
-  {
-    path: '/page-four',
-    component: Vue.component('page-four', require('./components/GestionPacientes.vue').default)
-  },
-  {
-    path: '/ingresarPaciente',
-    component: Vue.component('ingresarPaciente', require('./components/IngresarPaciente.vue').default)
-  },
-  {
-    path: '/EditarPaciente',
-    component: Vue.component('EditarPaciente', require('./components/EditarPaciente.vue').default)
-  },
-  {
-    path: '/EditarEstadoPaciente',
-    component: Vue.component('EditarEstadoPaciente', require('./components/EditarEstadoPaciente.vue').default)
-  },
-  {
-    path: '/Citas',
-    component: Vue.component('creacion-citas', require('./components/CreacionCitas.vue').default)
-  }
->>>>>>> 8c0e781faa97b98bdcc6e1afc6629db74f5639af
 ];
 
 const router = new VueRouter({
