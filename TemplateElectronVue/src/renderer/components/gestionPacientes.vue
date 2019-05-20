193f31710f06bc5efb6d0ddc32df626401abd6ab
Ana Hernandez <her17138@uvg.edu.gt>
2019-05-20 03:29:58 -0600

    Update gestionPacientes.vue

--- TemplateElectronVue/src/renderer/components/gestionPacientes.vue
+++ TemplateElectronVue/src/renderer/components/gestionPacientes.vue
@@ -167,10 +167,10 @@ export default {
       //esto deberia ser un arrray de pacientes que contengan todos sus atributos...
       
       this.pacientes = response.data.Pacientes;
-      //this.selectedPatients = response.data.Pacientes[0];
+      this.selectedPatients = response.data.Pacientes[0];
       //console.log(this.selectedPatients = response.data.Pacientes);
-      //this.Nombre = response.data.Pacientes[0].Nombre;
-      //this.Apellido = response.data.Pacientes[0].Apellido;
+      this.Nombre = response.data.Pacientes[0].Nombre;
+      this.Apellido = response.data.Pacientes[0].Apellido;
     });
     this.$http.get(`http://localhost:8000/EstadoController/getAllEstado`).then(response =>{
       this.estados_response = response.data.Estados;
