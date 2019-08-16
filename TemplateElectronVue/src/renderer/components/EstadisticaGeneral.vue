9d2d05f1f1a7f1de5346405716d3f075813d8e85
josepablito16 <josepablocif16@gmail.com>
2019-08-15 20:28:17 -0600

    Inicio de la pantalla estadistica general

--- TemplateElectronVue/src/renderer/components/EstadisticaGeneral.vue
+++ TemplateElectronVue/src/renderer/components/EstadisticaGeneral.vue
@@ -2,54 +2,160 @@
 	<div onload="obtenerNombre">
         <b-container class="containerMenu">
 
-            <!-- FILA 1, FOTOS Y TÍTULO-->
+            <!-- FILA 1 variable a estudiar-->
             <b-row align-h="around" align-v="center">
-
-                <b-col order="1" cols="1">
-                    <img src="src\temp\prfl.jpeg" alt="" width="120" height="120">
+                <b-col>
+                    <h2>Ingrese la variable a estudiar:</h2>
                 </b-col>
-
-                <b-col order="1" cols="6">
-                    <h2>BIENVENIDO</h2>
-                    <h4 id="nombre" type="text" v-model="name">{{name}}</h4>
+                
+                <b-col>
+                    <v-combobox
+                        v-model="varEstudio"
+                        :items="posiblesVariables"
+                        :search-input.sync="search"
+                        :hide-selected="hideSelected"
+                        label="Seleccione las variables a estudiar."
+                        :multiple="multiple"
+                        persistent-hint
+                        :small-chips="chips"
+                        :clearable="clearable"
+                    >
+                        <template v-if="noData" v-slot:no-data>
+                            <v-list-item>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                No se encontró "<strong>{{ search }}</strong>".
+                                </v-list-item-title>
+                            </v-list-item-content>
+                            </v-list-item>
+                        </template>
+                    </v-combobox>
                 </b-col>
+            </b-row>
 
-                <b-col order="1" cols="3" class="pb-4">
-                    <img src="../assets/mariposaFundanier.png" alt="" width="240" height="240">
+            <br>
+            <!-- FILA 2 condiciones-->
+            <b-row align-h="around" align-v="center">
+                <b-col>
+                    <h2>Ingrese las condiciones:</h2>
                 </b-col>
+                
+                <b-col>
+                    <v-combobox
+                        v-model="condiciones"
+                        :items="posiblesVariables"
+                        :search-input.sync="search"
+                        :hide-selected="hideSelected"
+                        label="Seleccione las variables a estudiar."
+                        :multiple="multiple"
+                        persistent-hint
+                        :small-chips="chips"
+                        :clearable="clearable"
+                    >
+                        <template v-if="noData" v-slot:no-data>
+                            <v-list-item>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                No se encontró "<strong>{{ search }}</strong>".
+                                </v-list-item-title>
+                            </v-list-item-content>
+                            </v-list-item>
+                        </template>
+                    </v-combobox>
+                </b-col>
+            </b-row>
+
+
+            <hr>
+
+            <b-row v-for="(a,index) in condiciones" :key="index">
+                <b-col>
+                    {{a}}
+                </b-col> 
+
+                <b-col>
+                    <v-select
+                    :items="simbolosCondiciones"
+                    label="Seleccione uno: "
+                    v-model="a.simbolo"
+                    ></v-select>
+                </b-col>    
+
+                <b-col>
+                    <v-text-field
+                        label="Solo"
+                        v-model="a.text"
+                        placeholder="Ingrese titulo"
+                        solo
+                    >
+                    </v-text-field>   
+                </b-col>             
 
             </b-row>
 
-            <!-- FILA 2, SET DE BOTONES-->
+            <hr>
+
+            <!-- FILA 3 agrupacion-->
+            <b-row align-h="around" align-v="center">
+                <b-col>
+                    <h2>Ingrese la variable de agrupacion:</h2>
+                </b-col>
+                
+                <b-col>
+                    <v-combobox
+                        v-model="agrupacion"
+                        :items="posiblesVariables"                
+                        label="Seleccione las variables de agrupacion."
+                    >
+                        <template v-if="noData" v-slot:no-data>
+                            <v-list-item>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                No se encontró "<strong>{{ search }}</strong>".
+                                </v-list-item-title>
+                            </v-list-item-content>
+                            </v-list-item>
+                        </template>
+
+                    </v-combobox>
+                </b-col>
+            </b-row>
+
+            <!-- FILA 2-->
             <b-row align-h="around" align-v="center" class="pb-2">
-                <b-col order="1" cols="9">
-                    <template>
-                    <div>
-                        <v-btn outline color="#303841" v-on:click="gestionarPaciente">Gestionar Paciente</v-btn>
-                        <v-btn outline color="#303841" v-on:click="darConsulta">Generar Reporte</v-btn>
-                        <v-btn outline color="#303841">Informes Estadísticos</v-btn>
-                        <v-btn outline color="#303841" v-on:click="datosGenerales">Datos Generales</v-btn>
-                    </div>
-                </template>
-                </b-col>
+                <v-btn outline color="#303841" v-on:click="prueba">Aceptar</v-btn>                
             </b-row>
+
+
+            <!-- Tabla-->
+            <b-row align-h="around" align-v="center" class="pb-2">        
+            </b-row>
+
+
+
         </b-container>
 	</div>
 </template>
 
 
 <script>
-// import { serverBus } from '../main';
 import { store } from "../main";
 var fecha = new Date();
 
 export default {
 
   data: () => ({
-    id: "",
-    name: "",
-    switch1: false,
-    colors: ["#FF9D14", "#BF760F", "#804F0A", "#402705", "#E68E12"]
+      posiblesVariables: ['Presion Arterial', 'Tipo de sango', 'Edad', 'Peso'],
+      search: null,
+      chips: true,
+      multiple: true,
+      hideSelected: true,
+      noData: true,
+      clearable: false,
+      varEstudio: [],
+      condiciones: [],
+      agrupacion: [],
+      simbolosCondiciones: ["=",">",">=","<","<=","diferente"]
   }),
 
   watch: {
@@ -63,6 +169,14 @@ export default {
   },
 
   methods: {
+      prueba()
+      {
+          
+          console.log(this.varEstudio);
+          console.log(this.agrupacion);
+          console.log(this.condiciones);
+          
+      }
 
     },
 
