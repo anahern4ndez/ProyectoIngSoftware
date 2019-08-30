d98cd5d35c99feb2fe4d7856c2bafec9ee195a5f
OJP98 <ojuarez_p@hotmail.com>
2019-08-30 01:54:28 -0600

    Scoped CSS

--- TemplateElectronVue/src/renderer/components/EstadisticaGeneral.vue
+++ TemplateElectronVue/src/renderer/components/EstadisticaGeneral.vue
@@ -79,13 +79,13 @@
                          v-on:click=""
                     >
                         <template v-if="noData" v-slot:no-data>
-                            <v-list-item>
+                            <!-- <v-list-item>
                             <v-list-item-content>
                                 <v-list-item-title>
                                 No se encontró "<strong>{{ search }}</strong>".
                                 </v-list-item-title>
                             </v-list-item-content>
-                            </v-list-item>
+                            </v-list-item> -->
                         </template>
                     </v-combobox>
                 </b-col>
@@ -277,35 +277,35 @@ export default {
 </script>
 
 
-<style>
-table {
-  font-family: 'Open Sans', sans-serif;
-  min-width: 900px;
-  border-collapse: collapse;
-  border: 3px solid #44475C;
-  margin: 10px 10px 0 10px;
-}
+<style scoped>
+    table {
+    font-family: 'Open Sans', sans-serif;
+    min-width: 900px;
+    border-collapse: collapse;
+    border: 3px solid #44475C;
+    margin: 10px 10px 0 10px;
+    }
 
-table th {
-  text-transform: uppercase;
-  text-align: left;
-  background: #44475C;
-  color: #FFF;
-  padding: 8px;
-  min-width: 30px;
-}
+    table th {
+    text-transform: uppercase;
+    text-align: left;
+    background: #44475C;
+    color: #FFF;
+    padding: 8px;
+    min-width: 30px;
+    }
 
-table td {
-  max-width: 100px;
-  text-align: left;
-  padding: 8px;
-  border-right: 2px solid #7D82A8;
-}
-table td:last-child {
-  border-right: none;
-}
-table tbody tr:nth-child(2n) td {
-  background: #D4D8F9;
+    table td {
+    max-width: 100px;
+    text-align: left;
+    padding: 8px;
+    border-right: 2px solid #7D82A8;
+    }
+    table td:last-child {
+    border-right: none;
+    }
+    table tbody tr:nth-child(2n) td {
+    background: #D4D8F9;
 }
 
 
