528e97f5469fd501e87388687202e8061ad1ccc2
OJP98 <ojuarez_p@hotmail.com>
2020-07-07 00:55:57 -0600

    Se crea CSV acorde a cada variable

--- TemplateElectronVue/src/renderer/components/EstadisticaGeneral.vue
+++ TemplateElectronVue/src/renderer/components/EstadisticaGeneral.vue
@@ -24,7 +24,7 @@
                     :items="posiblesTiempos"
                     v-on:change="changeDisableTiempo"
                     v-model="selectTiempo"
-                    label="Elija el formulario que desea subir">
+                    label="Elija el tiempo de estudio">
                     </v-combobox>
                     <br>
                     <input type="file" class="hide_file" style="height:auto; width:auto; visibility:hidden" v-on:change="changeForm" ref="changeForm"/>
@@ -39,17 +39,13 @@
 <script>
 import { store } from "../main";
 import { log } from "util";
+import { resolve } from "dns";
+import { rejects } from "assert";
 var fecha = new Date();
 
 export default {
   data: () => ({
-    posiblesVariables: [
-      "Acciones",
-      "Consultas",
-      "Datos generales",
-      "Pacientes",
-      "Citas"
-    ],
+    posiblesVariables: ["Acciones", "Consultas", "Pacientes", "Citas"],
     posiblesTiempos: [
       "Mes pasado",
       "3 meses",
@@ -64,32 +60,39 @@ export default {
     isDisabledAbrir: true,
     isDisabledTiempo: true,
     isDisabledButton: true,
-    nombreArchivo: "Citas.csv"
+    nombreArchivo: " "
   }),
 
   methods: {
     startExcel() {
-      this.crearCSVEnServer(this.nombreArchivo);
+      this.nombreArchivo = `${this.selectVariable}.csv`;
 
-      this.isDisabledSubir = false;
-      this.isDisabledSubirForm = false;
-      this.exit = false;
+      this.crearCSVEnServer(this.nombreArchivo)
+        .then(response => {
+          this.isDisabledSubir = false;
+          this.isDisabledSubirForm = false;
+          this.exit = false;
 
-      const { shell } = require("electron");
-      var fs = require("fs");
-      var dir = process.cwd();
+          const { shell } = require("electron");
+          var fs = require("fs");
+          var dir = process.cwd();
 
-      dir = dir.concat("\\src\\CSVs\\", this.nombreArchivo);
-      // console.log(dir)
-      shell.openItem(dir);
+          dir = dir.concat("\\src\\CSVs\\", this.nombreArchivo);
+          // console.log(dir)
+          shell.openItem(dir);
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
 
-    crearCSVEnServer(nombreArchivo) {
+    crearCSVEnServer() {
       // Constantes útiles
       const { Pool, Client } = require("pg");
       const fastcsv = require("fast-csv");
       const fs = require("fs");
-      const ws = fs.createWriteStream(nombreArchivo);
+      const pathArchivo = `src/CSVs/${this.nombreArchivo}`;
+      const ws = fs.createWriteStream(pathArchivo);
 
       // Se crea el cliente con configuración del servidor
       const client = new Client({
@@ -100,24 +103,139 @@ export default {
         port: 5432
       });
 
+      let query;
+
+      if (this.selectVariable === "Acciones") {
+        query = `
+        SELECT 
+          name as "Nombre del usuario", 
+          fecha as "Fecha de la acción",
+          "TipoFormulario" as "Tipo de acción", 
+          "Nombre" as "Nombre del paciente", 
+          "Apellido" as "Apellido del paciente",
+          hist."CUI" as "CUI del paciente"
+        FROM 
+          historiales hist
+        INNER JOIN pacientes ON pacientes."CUI" = hist."CUI"
+        INNER JOIN users ON CAST (users.id AS Character) = hist."IdDoctor"`;
+      } else if (this.selectVariable === "Citas") {
+        query = `
+        SELECT 
+          name as "Nombre del usuario",
+          fecha "Fecha de la cita",
+          hora as "Hora Inicio",
+          "duracionCita" as "Duración de la cita",
+          "Nombre" as "Nombre del paciente", 
+          "Apellido" as "Apellido del paciente"
+        FROM citas
+        INNER JOIN users u ON u.id = citas."idUsuario"
+        INNER JOIN pacientes ON pacientes.id = citas."idPaciente"`;
+      } else if (this.selectVariable === "Consultas") {
+        query = `
+        SELECT
+          "Nombre" as "Nombre del paciente", 
+          "Apellido" as "Apellido del paciente",
+          cui as "CUI",
+          fecha as "Fecha consulta",
+          peso, talla, pa as "Presión arterial",
+          sindrome_clinico,
+          historia,
+          evaluacion_medica,
+          plan_medico,
+          evaluacion_psicologica,
+          plan_psicologico,
+          evaluacion_trabajo_social,
+          plan_trabajo_social,
+          evaluacion_nutricional,
+          plan_nutricional,
+          evaluacion_farmacologica,
+          plan_farmacologico
+        FROM consultas c
+        INNER JOIN pacientes ON pacientes."CUI" = c.cui`;
+      } else if (this.selectVariable === "Pacientes") {
+        query = `
+        SELECT 
+          "Numero_expediente",
+          "CUI",
+          "Nombre",
+          "Apellido",
+          "Fecha_de_nacimiento",
+          "Procedencia",
+          "Residencia",
+          "Nombre_de_padre",
+          "Nombre_de_madre",
+          "Telefono"
+          "Edad",
+          "Historia"
+        FROM pacientes`;
+      }
+
+      let tiempo;
+      const fechaActual = new Date();
+
+      if (this.selectTiempo === "Mes pasado") {
+        tiempo = new Date(
+          fechaActual.getFullYear(),
+          fechaActual.getMonth() - 1,
+          fechaActual.getDate()
+        );
+      } else if (this.selectTiempo === "3 meses") {
+        tiempo = new Date(
+          fechaActual.getFullYear(),
+          fechaActual.getMonth() - 3,
+          fechaActual.getDate()
+        );
+      } else if (this.selectTiempo === "6 meses") {
+        tiempo = new Date(
+          fechaActual.getFullYear(),
+          fechaActual.getMonth() - 6,
+          fechaActual.getDate()
+        );
+      } else if (this.selectTiempo === "1 año") {
+        tiempo = new Date(
+          fechaActual.getFullYear() - 1,
+          fechaActual.getMonth(),
+          fechaActual.getDate()
+        );
+      } else if (this.selectTiempo === "2 años") {
+        tiempo = new Date(
+          fechaActual.getFullYear() - 2,
+          fechaActual.getMonth(),
+          fechaActual.getDate()
+        );
+      } else {
+        tiempo = new Date(
+          fechaActual.getFullYear() - 30,
+          fechaActual.getMonth(),
+          fechaActual.getDate()
+        );
+      }
+
+      if (this.selectVariable !== "Pacientes") {
+        const condicionTiempo = `
+        WHERE fecha >= '${tiempo.getFullYear()}-${tiempo.getMonth()}-${tiempo.getDate()}'`;
+        query = query + condicionTiempo;
+      }
+
       // Conexión a servidor
       client.connect();
 
-      const query = "SELECT * FROM users";
+      return new Promise((resolve, rejects) => {
+        client.query(query, (err, res) => {
+          const jsonData = JSON.parse(JSON.stringify(res.rows));
+          // Escribir el csv
+          fastcsv
+            .write(jsonData, { headers: true })
+            .on("finish", function() {
+              console.log("CSV fue creado con éxito usando datos del servidor");
+            })
+            .pipe(ws);
 
-      client.query(query, (err, res) => {
-        const jsonData = JSON.parse(JSON.stringify(res.rows));
+          // Terminar conexión con el servidor
+          client.end();
+        });
 
-        // Escribir el csv
-        fastcsv
-          .write(jsonData, { headers: true })
-          .on("finish", function() {
-            console.log("CSV fue creado con éxito usando datos del servidor");
-          })
-          .pipe(ws);
-
-        // Terminar conexión con el servidor
-        client.end();
+        resolve("success!");
       });
     },
     changeDisableVariable(event) {
